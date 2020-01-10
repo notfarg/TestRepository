ef85f9d43ebec9c8444ab0ed5996cb4529870405
notfarg <eliza.m.grafton@gmail.com>
2020-01-10 10:00:35 -0500

    First scripts added

---
+++ BlankProject/Assets/Scripts/BlankScript1.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlankScript1 : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public string username = "Eliza";
+    public int health = 200;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        print(username);
+    }
+}
